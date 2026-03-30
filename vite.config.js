eebdcdac8950494d0c7e99a137ee90700ae82d99
togosuzuki820-source <togosuzuki820@gmail.com>
2026-03-30 17:07:38 +0900

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // ★重要：あなたのGitHubリポジトリ名を / で囲んで書いてください
+  // 例：リポジトリ名が swahili-app なら '/swahili-app/'
+  base: '/togosuzuki820-source.github.io/', 
+})
